1e41852360651961848767cb2e0313497025e984
wei huang <weimpromptu@gmail.com>
2021-07-25 20:16:43 -0400

    header minor correction

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,15 +5,6 @@ export default function NavBar() {
     <Navbar bg="light" expand="lg">
       <Container>
         <Navbar.Brand href="/" className="text-primary">Shepherd </Navbar.Brand>
-        {/* <Navbar.Toggle /> */}
-        {/* <Navbar.Collapse className="justify-content-end">
-          <Nav className="me-auto">
-            <Nav.Link href="http://localhost:3000/">Home</Nav.Link>
-            <Nav.Link href="/login" className="justify-content-end">Log In</Nav.Link>
-            <Nav.Link href="/login" className="justify-content-end">Log In</Nav.Link>
-          </Nav>
-        </Navbar.Collapse> */}
-        {/* <Navbar.Toggle /> */}
         <Navbar.Collapse className="justify-content-end">
           <NavLink href="/signup">
             Sign Up
